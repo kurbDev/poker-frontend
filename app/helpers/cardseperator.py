e7ca936bbb4e8ab97f3c28b05fcc8e399b833640
HorrorEmpire <113548417+HorrorEmpire@users.noreply.github.com>
2024-12-24 23:37:31 +0100

    gitignore update

--- app/helpers/cardseperator.py
+++ app/helpers/cardseperator.py
@@ -16,10 +16,10 @@ card_width = image_width // cols
 card_height = image_height // rows
 
 # Refine the padding adjustments
-left_padding = 21    # Less padding on the left to remove white space
-right_padding = 10   # Adjusted to avoid cutting off the card's edge
-top_padding = 16     # Minor adjustment for the top white space
-bottom_padding = 15  # Adjusted for the bottom white space
+left_padding = 21   
+right_padding = 10 
+top_padding = 16
+bottom_padding = 15 
 
 # Directory to save individual cards
 output_dir = "./cards/"
@@ -32,14 +32,11 @@ file_paths = []
 
 for row in range(rows):
     for col in range(cols):
-        # Calculate cropping coordinates
         left = col * card_width + left_padding
         upper = row * card_height + top_padding
         right = (col + 1) * card_width - right_padding
         lower = (row + 1) * card_height - bottom_padding
 
-        
-        # Crop the card
         card = original_image.crop((left, upper, right, lower))
         
         # Name cards appropriately
@@ -57,9 +54,7 @@ for row in range(rows):
         
         card_name = f"{rank_name}_of_{card_names[row]}.png"
         card_path = os.path.join(output_dir, card_name)
-        file_paths.append(card_path)
-        
-        # Save the card
+        file_paths.append(card_path)        
         card.save(card_path)
 
 file_paths
